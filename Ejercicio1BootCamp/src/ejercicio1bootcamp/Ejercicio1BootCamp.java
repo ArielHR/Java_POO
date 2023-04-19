41f6bcb86249506675baebc5e983dcd5e6f81c68
ArielHR <arielhr_09@hotmail.com>
2023-04-19 11:25:39 -0600

    Primer ejercicio de Boot Camp

---
+++ Ejercicio1BootCamp/src/ejercicio1bootcamp/Ejercicio1BootCamp.java
@@ -0,0 +1,23 @@
+
+package ejercicio1bootcamp;
+
+
+public class Ejercicio1BootCamp {
+
+
+    public static void main(String[] args) {
+     
+        int enteros = 24;
+        long decimalesL = 345675554/enteros;
+        double decimalesD = 4.67854312;
+        boolean verificador = enteros < decimalesL;
+        String cadena = "Hello Wordl";
+        
+        System.out.println(enteros);
+        System.out.println(decimalesL);
+        System.out.println(decimalesD);
+        System.out.println(verificador);
+        System.out.println(cadena);
+    }
+    
+}
